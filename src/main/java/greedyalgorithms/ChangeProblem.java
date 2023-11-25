b40c18e733ef6eb6b70591be12713dbb7c4dd9b8
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2023-11-25 15:46:12 +0100

    feat: change variable name to more convenient. Methods now use const, instead of magical numbers.

--- src/main/java/greedyalgorithms/ChangeProblem.java
+++ src/main/java/greedyalgorithms/ChangeProblem.java
@@ -4,31 +4,32 @@ import java.util.List;
 
 public class ChangeProblem {
 
+    private static final List<Integer> DENOMINATIONS_1 = List.of(1, 5, 10);
+    private static final List<Integer> DENOMINATIONS_2 = List.of(1, 4, 6);
+
     public static void main(String[] args) {
 
-        List<Integer> denominations = List.of(1, 5, 10);
-        List<Integer> denominations2 = List.of(1, 4, 6);
         int sumToPay = 28;
         int sumToPay2 = 8;
 
-        System.out.println(getNumberOfCoins(sumToPay, denominations));
-        System.out.println(getNumberOfCoins(sumToPay2, denominations2));
-
-        System.out.println(getNumberOfCoinsRecursively(sumToPay, denominations));
+        System.out.println(getNumberOfCoins(sumToPay, DENOMINATIONS_1));
+        System.out.println(getNumberOfCoins(sumToPay2, DENOMINATIONS_2));
+        System.out.println(getNumberOfCoinsRecursively(sumToPay, DENOMINATIONS_1));
+        System.out.println(getNumberOfCoinsGreedily(sumToPay));
 
     }
 
-    private static int getNumberOfCoins(int sumToPay, List<Integer> denominations) {
+    private static int getNumberOfCoins(int sum, List<Integer> denominations) {
         int numCoin = 0;
 
-        while (sumToPay > 0) {
-            int finalSumToPay = sumToPay;
+        while (sum > 0) {
+            int finalSumToPay = sum;
             int maxCoin = denominations.stream()
                     .filter(coin -> finalSumToPay - coin >= 0)
                     .max(Integer::compare)
                     .orElse(1);
 
-            sumToPay -= maxCoin;
+            sum -= maxCoin;
             numCoin += 1;
         }
 
@@ -45,6 +46,24 @@ public class ChangeProblem {
                 .orElse(1);
 
         return 1 + getNumberOfCoinsRecursively(sum - maxCoin, denominations);
+    }
+
+    private static int getNumberOfCoinsGreedily(int sum) {
+        int numCoin = 0;
+
+        while (sum > 0) {
+            if (sum >= DENOMINATIONS_1.get(2)) {
+                sum -= DENOMINATIONS_1.get(2);
+            } else if (sum >= DENOMINATIONS_1.get(1)) {
+                sum -= DENOMINATIONS_1.get(1);
+            } else {
+                sum -= DENOMINATIONS_1.get(0);
+            }
+
+            numCoin++;
+        }
+
+        return numCoin;
     }
 
 }
